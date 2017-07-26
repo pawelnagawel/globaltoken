aace3d3bf089910eab6951b28dedf95b3aa8f77e
globaltoken <heisenbergakatheking@gmail.com>
2017-07-27 00:15:56 +0200

    Mac OSX Logo issue fixxed
    Mac OSX Logo issue fixxed

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.globaltoken.Globaltoken-Qt";
     } else {
         return [self __bundleIdentifier];
     }
