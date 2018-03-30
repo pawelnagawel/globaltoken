460d4862c13a8b89d790e3de1950ac0a80d94977
e19293001 <e19293001@gmail.com>
2018-03-30 20:51:17 +0800

    Revert "added implementation for uint512"
    This reverts commit 33c23811b30daa769c83822a2e349fa0879e5223.

--- src/uint256.h
+++ src/uint256.h
@@ -42,191 +42,11 @@ public:
         memset(data, 0, sizeof(data));
     }
 
-	inline int Compare(const base_blob& other) const { return memcmp(data, other.data, sizeof(data)); }
-	
-    base_blob& operator=(const base_blob& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            data[i] = b.data[i];
-        return *this;
-    }
+    inline int Compare(const base_blob& other) const { return memcmp(data, other.data, sizeof(data)); }
 
-    base_blob(uint64_t b)
-    {
-        data[0] = (unsigned int)b;
-        data[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            data[i] = 0;
-    }
-
-    explicit base_blob(const std::string& str);
-    //explicit base_blob(const std::vector<unsigned char>& vch);
-
-    bool operator!() const
-    {
-        for (int i = 0; i < WIDTH; i++)
-            if (data[i] != 0)
-                return false;
-        return true;
-    }
-
-    const base_blob operator~() const
-    {
-        base_blob ret;
-        for (int i = 0; i < WIDTH; i++)
-            ret.data[i] = ~data[i];
-        return ret;
-    }
-
-    const base_blob operator-() const
-    {
-        base_blob ret;
-        for (int i = 0; i < WIDTH; i++)
-            ret.data[i] = ~data[i];
-        ret++;
-        return ret;
-    }
-
-    double getdouble() const;
-
-    base_blob& operator=(uint64_t b)
-    {
-        data[0] = (unsigned int)b;
-        data[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            data[i] = 0;
-        return *this;
-    }
-
-    base_blob& operator^=(const base_blob& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            data[i] ^= b.data[i];
-        return *this;
-    }
-
-    base_blob& operator&=(const base_blob& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            data[i] &= b.data[i];
-        return *this;
-    }
-
-    base_blob& operator|=(const base_blob& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            data[i] |= b.data[i];
-        return *this;
-    }
-
-    base_blob& operator^=(uint64_t b)
-    {
-        data[0] ^= (unsigned int)b;
-        data[1] ^= (unsigned int)(b >> 32);
-        return *this;
-    }
-
-    base_blob& operator|=(uint64_t b)
-    {
-        data[0] |= (unsigned int)b;
-        data[1] |= (unsigned int)(b >> 32);
-        return *this;
-    }
-
-    base_blob& operator<<=(unsigned int shift);
-    base_blob& operator>>=(unsigned int shift);
-
-    base_blob& operator+=(const base_blob& b)
-    {
-        uint64_t carry = 0;
-        for (int i = 0; i < WIDTH; i++) {
-            uint64_t n = carry + data[i] + b.data[i];
-            data[i] = n & 0xffffffff;
-            carry = n >> 32;
-        }
-        return *this;
-    }
-
-    base_blob& operator-=(const base_blob& b)
-    {
-        *this += -b;
-        return *this;
-    }
-
-    base_blob& operator+=(uint64_t b64)
-    {
-        base_blob b;
-        b = b64;
-        *this += b;
-        return *this;
-    }
-
-    base_blob& operator-=(uint64_t b64)
-    {
-        base_blob b;
-        b = b64;
-        *this += -b;
-        return *this;
-    }
-
-    base_blob& operator*=(uint32_t b32);
-    base_blob& operator*=(const base_blob& b);
-    base_blob& operator/=(const base_blob& b);
-
-    base_blob& operator++()
-    {
-        // prefix operator
-        int i = 0;
-        while (++data[i] == 0 && i < WIDTH - 1)
-            i++;
-        return *this;
-    }
-
-    const base_blob operator++(int)
-    {
-        // postfix operator
-        const base_blob ret = *this;
-        ++(*this);
-        return ret;
-    }
-
-    base_blob& operator--()
-    {
-        // prefix operator
-        int i = 0;
-        while (--data[i] == (uint32_t)-1 && i < WIDTH - 1)
-            i++;
-        return *this;
-    }
-
-    const base_blob operator--(int)
-    {
-        // postfix operator
-        const base_blob ret = *this;
-        --(*this);
-        return ret;
-    }
-    int CompareTo(const base_blob& b) const;
-    bool EqualTo(uint64_t b) const;
-
-    friend inline const base_blob operator+(const base_blob& a, const base_blob& b) { return base_blob(a) += b; }
-    friend inline const base_blob operator-(const base_blob& a, const base_blob& b) { return base_blob(a) -= b; }
-    friend inline const base_blob operator*(const base_blob& a, const base_blob& b) { return base_blob(a) *= b; }
-    friend inline const base_blob operator/(const base_blob& a, const base_blob& b) { return base_blob(a) /= b; }
-    friend inline const base_blob operator|(const base_blob& a, const base_blob& b) { return base_blob(a) |= b; }
-    friend inline const base_blob operator&(const base_blob& a, const base_blob& b) { return base_blob(a) &= b; }
-    friend inline const base_blob operator^(const base_blob& a, const base_blob& b) { return base_blob(a) ^= b; }
-    friend inline const base_blob operator>>(const base_blob& a, int shift) { return base_blob(a) >>= shift; }
-    friend inline const base_blob operator<<(const base_blob& a, int shift) { return base_blob(a) <<= shift; }
-    friend inline const base_blob operator*(const base_blob& a, uint32_t b) { return base_blob(a) *= b; }
-    friend inline bool operator==(const base_blob& a, const base_blob& b) { return memcmp(a.data, b.data, sizeof(a.data)) == 0; }
-    friend inline bool operator!=(const base_blob& a, const base_blob& b) { return memcmp(a.data, b.data, sizeof(a.data)) != 0; }
-    friend inline bool operator>(const base_blob& a, const base_blob& b) { return a.CompareTo(b) > 0; }
-    friend inline bool operator<(const base_blob& a, const base_blob& b) { return a.CompareTo(b) < 0; }
-    friend inline bool operator>=(const base_blob& a, const base_blob& b) { return a.CompareTo(b) >= 0; }
-    friend inline bool operator<=(const base_blob& a, const base_blob& b) { return a.CompareTo(b) <= 0; }
-    friend inline bool operator==(const base_blob& a, uint64_t b) { return a.EqualTo(b); }
-    friend inline bool operator!=(const base_blob& a, uint64_t b) { return !a.EqualTo(b); }
+    friend inline bool operator==(const base_blob& a, const base_blob& b) { return a.Compare(b) == 0; }
+    friend inline bool operator!=(const base_blob& a, const base_blob& b) { return a.Compare(b) != 0; }
+    friend inline bool operator<(const base_blob& a, const base_blob& b) { return a.Compare(b) < 0; }
 
     std::string GetHex() const;
     void SetHex(const char* psz);
@@ -282,10 +102,6 @@ public:
     {
         s.read((char*)data, sizeof(data));
     }
-
-    friend class uint160;
-    friend class uint256;
-    friend class uint512;
 };
 
 /** 160-bit opaque blob.
@@ -340,30 +156,4 @@ inline uint256 uint256S(const std::string& str)
     return rv;
 }
 
-/** 512-bit unsigned big integer. */
-class uint512 : public base_blob<512>
-{
-public:
-    uint512() {}
-    uint512(const base_blob<512>& b) : base_blob<512>(b) {}
-    uint512(uint64_t b) : base_blob<512>(b) {}
-    explicit uint512(const std::string& str) : base_blob<512>(str) {}
-    explicit uint512(const std::vector<unsigned char>& vch) : base_blob<512>(vch) {}
-
-    uint256 trim256() const
-    {
-        uint256 ret;
-        for (unsigned int i = 0; i < uint256::WIDTH; i++) {
-            ret.data[i] = data[i];
-        }
-        return ret;
-    }
-};
-
-inline uint512 uint512S(const std::string& str)
-{
-    uint512 rv;
-    rv.SetHex(str);
-    return rv;
-}
 #endif // BITCOIN_UINT256_H
