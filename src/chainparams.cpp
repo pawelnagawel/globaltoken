610064c6419079f2f0e55592e5077579cefdc088
globaltoken <admin@globaltoken.org>
2018-04-23 00:41:30 +0200

    Removed unnecessary includes
    - Removed unnecessary includes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -7,9 +7,6 @@
 #include <chainparams.h>
 #include <consensus/merkle.h>
 
-#include <uint256.h>
-#include <arith_uint256.h>
-
 #include <tinyformat.h>
 #include <util.h>
 #include <utilstrencodings.h>
