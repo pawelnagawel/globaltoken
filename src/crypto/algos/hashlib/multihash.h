8bd669d640c0b2d3c55721f042b3a72c582d314c
globaltoken <globaltoken@users.noreply.github.com>
2018-04-22 20:00:10 +0200

    Update multihash.h
    - Bugfix

--- src/crypto/algos/hashlib/multihash.h
+++ src/crypto/algos/hashlib/multihash.h
@@ -540,23 +540,23 @@ inline uint256 NIST5(const T1 pbegin, const T1 pend)
 
     sph_blake512_init(&ctx_blake);
     sph_blake512(&ctx_blake, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
-    sph_blake512_close(&ctx_blake, static_cast<const void*>(&hash[0]));
+    sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[0]));
 
     sph_groestl512_init(&ctx_groestl);
     sph_groestl512(&ctx_groestl, static_cast<const void*>(&hash[0]), 80);
-    sph_groestl512_close(&ctx_groestl, static_cast<const void*>(&hash[1]));
+    sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[1]));
 
     sph_jh512_init(&ctx_jh);
     sph_jh512(&ctx_jh, static_cast<const void*>(&hash[1]), 64);
-    sph_jh512_close(&ctx_jh, static_cast<const void*>(&hash[2]));
+    sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[2]));
 
     sph_keccak512_init(&ctx_keccak);
     sph_keccak512(&ctx_keccak, static_cast<const void*>(&hash[2]), 64);
-    sph_keccak512_close(&ctx_keccak, static_cast<const void*>(&hash[3]));
+    sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[3]));
 
     sph_skein512_init(&ctx_skein);
     sph_skein512(&ctx_skein, static_cast<const void*>(&hash[3]), 64);
-    sph_skein512_close(&ctx_skein, static_cast<const void*>(&hash[4]));
+    sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[4]));
 
     return hash[4].trim256();
 }
