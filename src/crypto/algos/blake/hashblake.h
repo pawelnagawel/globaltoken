a474d90f2ae6d139ed3c86034792c65dac85d389
globaltoken <admin@globaltoken.org>
2018-06-19 00:22:11 +0200

    Fixed Blake2B
    - Blake2B crashed because it generated an 64 byte Output. We moved it to
    uint 64bytes, so it works now.
    - Blake2B works in theory now, but must be tested to make sure if it
    works in the blockchain together with existing ASIC Blake2B Miners.

--- src/crypto/algos/blake/hashblake.h
+++ src/crypto/algos/blake/hashblake.h
@@ -25,12 +25,12 @@ template<typename T1>
 inline uint256 HashBlake2B(const T1 pbegin, const T1 pend)
 {
     static unsigned char pblank[1];
-    uint256 hash1;
+    uint512 hash1;
     blake2b_state S[1];
     blake2b_init( S, BLAKE2B_OUTBYTES );
     blake2b_update( S, (pbegin == pend ? pblank : (unsigned char*)&pbegin[0]), (pend - pbegin) * sizeof(pbegin[0]) );
     blake2b_final( S, (unsigned char*)&hash1, BLAKE2B_OUTBYTES );
-    return hash1;
+    return hash1.trim256();
 }
 
 #endif // HASH_BLAKE_H
