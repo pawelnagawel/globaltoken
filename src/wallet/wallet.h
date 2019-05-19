e9a60a2698e435d2b21dc269e561a4b5c9633388
globaltoken <admin@globaltoken.org>
2019-05-19 13:49:20 +0200

    Added small fees for transactions
    - Reduced the tx fees for now.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -44,7 +44,7 @@ extern bool fWalletRbf;
 
 static const unsigned int DEFAULT_KEYPOOL_SIZE = 1000;
 //! -paytxfee default
-static const CAmount DEFAULT_TRANSACTION_FEE = 0;
+static const CAmount DEFAULT_TRANSACTION_FEE = 1000; // for now we reduce the tx fees in GlobalToken network.
 //! -fallbackfee default
 static const CAmount DEFAULT_FALLBACK_FEE = 20000;
 //! -m_discard_rate default
